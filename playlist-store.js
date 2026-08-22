8740db4f027383ff61d9b30742889267c7f89201
Kevin <kjwagner613@gmail.com>
2026-08-22 15:55:49 -0700

    feat: rename Family built-in playlist

--- playlist-store.js
+++ playlist-store.js
@@ -18,7 +18,7 @@ const PlaylistStore = (() => {
     },
     {
       id: "songs",
-      name: "Shared Playlist 2",
+      name: "Family Playlist",
       source: () => (typeof songs !== "undefined" && Array.isArray(songs) ? songs : [])
     }
   ];
